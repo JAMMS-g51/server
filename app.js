20e7302ec3e854100745d9a98789940a7d26709f
Matt Pitts <mattpittsmusic@gmail.com>
2017-06-24 10:48:57 -0600

    express app generated, dependencies brought in

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cors){
+	credentials: true,
+	origin: process.env.CORS_ORIGIN
+})):
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+	  message: err.message,
+	  error: req.app.get('env') === 'development' ? err : {}
+  });
+});
+
+module.exports = app;
